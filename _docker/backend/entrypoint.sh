7b404b2140e63613754611e423aef030a770672e
burn2705 <shevvg2004@yandex.ru>
2024-05-26 16:48:42 +0300

    cleaned up backend confs

--- _docker/backend/entrypoint.sh
+++ _docker/backend/entrypoint.sh
@@ -1,4 +1,16 @@
-# crond -bS # -L logs
-crond -bS & # & -L logs
-cd /var/www/html/$BRANCH
-php artisan serve
+
+
+SRV_PATH=/var/www/html/
+mkdir /var/log/server
+PHP_LOG_NAME=/var/log/server/php_$(date +"%d-%m-%Y")
+CRON_LOG_NAME=/var/log/server/cron_$(date +"%d-%m-%Y")
+CROND_PATH=/
+
+cd /var/log/server
+touch ${CRON_LOG_NAME}
+touch ${PHP_LOG_NAME}
+
+# crond -fS -L 0 -d 0 -c ${CROND_PATH} > ${LOG_NAME} &
+crond -fS -L 0 -d 0 > ${CRON_LOG_NAME} &
+cd ${SRV_PATH} && php artisan serve  && fg > ${PHP_LOG_NAME} 
+# nohup crond -bS -L 0 -d 0 -c "/etc/periodic" & nohup php artisan serve  && fg
--- _docker/postgres_dev/entrypoint.sh
+++ _docker/postgres_dev/entrypoint.sh
@@ -26,7 +26,9 @@ int_handler() {
     touch /var/log/postgresql/dump.log
     EXPORT_PATH = /opt/dumps/
     echo "dumping database" > /var/log/postgresql/dump.log
-    su postgres -c "pg_dump localhost databasename >> ${EXPORT_PATH}bs_dump_$(date +%Y-%m-%d-%H.%M.%S).sql" \
+    # su postgres -c "pg_dump borealstar >> ${EXPORT_PATH}bs_dump_$(date +%Y-%m-%d-%H.%M.%S).sql" \
+    # pg_dump borealstar -U docker >> /opt/dumps/bs_dump_$(date +%Y-%m-%d-%H.%M.%S).sql
+    pg_dump borealstar -U docker >> /opt/dumps/bs_dump_$(date +%Y-%m-%d-%H.%M.%S).sql
         > /var/log/postgresql/dump.log
 
 #   node deactivator & pid="$!"
@@ -36,14 +38,18 @@ int_handler() {
 trap int_handler SIGKILL
 trap int_handler SIGINT
 
+
+postgres
+
+
 # ls -l /etc/ | grep "postgre"
 # ls -l /etc/ | grep "postgre"
 
 # su -c "rm -rf /var/lib/postgresql/data"
-echo "psql"
-ls -la /var/lib/postgresql
-echo "datadir"
-ls -la /var/lib/postgresql/data
+# echo "psql"
+# ls -la /var/lib/postgresql
+# echo "datadir"
+# ls -la /var/lib/postgresql/data
 # cat /var/lib/postgresql/data/*
 
 # su postgres -c "postgres \
