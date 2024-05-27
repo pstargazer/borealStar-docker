9f6f95ce244af0f4dab2656cc5496b5b766ff614
burn2705 <shevvg2004@yandex.ru>
2024-05-27 22:27:07 +0300

    fixed alpine bootstrap

--- _docker/backend/entrypoint.sh
+++ _docker/backend/entrypoint.sh
@@ -1,16 +1,16 @@
 
 
-SRV_PATH=/var/www/html/
+SRV_PATH=$CMD_PATH
 mkdir /var/log/server
-PHP_LOG_NAME=/var/log/server/php_$(date +"%d-%m-%Y")
-CRON_LOG_NAME=/var/log/server/cron_$(date +"%d-%m-%Y")
-CROND_PATH=/
+export PHP_LOG_NAME=/var/log/server/php_$(date +"%d-%m-%Y")
+export CRON_LOG_NAME=/var/log/server/cron_$(date +"%d-%m-%Y")
+CROND_PATH=$CRON_LOG_NAME
 
 cd /var/log/server
 touch ${CRON_LOG_NAME}
 touch ${PHP_LOG_NAME}
 
 # crond -fS -L 0 -d 0 -c ${CROND_PATH} > ${LOG_NAME} &
-crond -fS -L 0 -d 0 > ${CRON_LOG_NAME} &
+crond -fS -L 0 -d 0 & #${CRON_LOG_NAME} &
 cd ${SRV_PATH} && php artisan serve  && fg > ${PHP_LOG_NAME} 
 # nohup crond -bS -L 0 -d 0 -c "/etc/periodic" & nohup php artisan serve  && fg
