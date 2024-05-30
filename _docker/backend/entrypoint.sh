6cca3b898ccc025b1ddfce3af022039290358956
niconicotin <shevvg2004@yandex.ru>
2024-05-30 16:36:21 +0300

    entrypoint installing deps now

--- _docker/backend/entrypoint.sh
+++ _docker/backend/entrypoint.sh
@@ -12,5 +12,14 @@ touch ${PHP_LOG_NAME}
 
 # crond -fS -L 0 -d 0 -c ${CROND_PATH} > ${LOG_NAME} &
 crond -fS -L 0 -d 0 & #${CRON_LOG_NAME} &
-cd ${SRV_PATH} && php artisan serve  && fg > ${PHP_LOG_NAME} 
+cd ${SRV_PATH} 
+pwd
+
+if [ -d ./vendor ]; then
+    echo "deps is okay"
+else
+    composer install 
+fi
+
+php artisan serve  && fg > ${PHP_LOG_NAME} 
 # nohup crond -bS -L 0 -d 0 -c "/etc/periodic" & nohup php artisan serve  && fg
